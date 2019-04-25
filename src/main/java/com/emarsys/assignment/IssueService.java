906ee63dd63816601d19f91a00ea42440914cbe7
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 14:08:21 +0200

    exception thrown if turnAroundTime is 0

--- src/main/java/com/emarsys/assignment/IssueService.java
+++ src/main/java/com/emarsys/assignment/IssueService.java
@@ -1,11 +1,19 @@
 package com.emarsys.assignment;
 
+import com.emarsys.assignment.exception.BadTurnAroundTime;
+
 import java.util.Calendar;
 
 public class IssueService {
 
     public Calendar calculateDueDate(Calendar submitDate, int turnAroundTime) {
-        return Calendar.getInstance();
+        if (turnAroundTime == 0) {
+            throw new BadTurnAroundTime("Bad turnaround time!");
+        }
+        Calendar calendar = Calendar.getInstance();
+        int turnAroundHour = submitDate.get(Calendar.HOUR_OF_DAY) + turnAroundTime;
+        calendar.set(Calendar.HOUR_OF_DAY, turnAroundHour);
+        return calendar;
     }
 
 }
