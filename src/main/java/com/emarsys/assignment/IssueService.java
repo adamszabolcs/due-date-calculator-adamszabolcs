b015cc7ea5847600a41ecb43dfd9dbea9faf5cf9
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 14:47:24 +0200

    refactor, created day and time variables

--- src/main/java/com/emarsys/assignment/IssueService.java
+++ src/main/java/com/emarsys/assignment/IssueService.java
@@ -8,14 +8,15 @@ import java.util.Calendar;
 public class IssueService {
 
     public Calendar calculateDueDate(Calendar submitDate, int turnAroundTime) {
+        int time = submitDate.get(Calendar.HOUR_OF_DAY);
+        int day = submitDate.get(Calendar.DAY_OF_WEEK);
         if (turnAroundTime <= 0) {
             throw new BadTurnAroundTime("Bad turnaround time!");
-        } else if (submitDate.get(Calendar.DAY_OF_WEEK) > 5 ||
-                (submitDate.get(Calendar.HOUR_OF_DAY) < 9 || submitDate.get(Calendar.HOUR_OF_DAY) > 17)) {
+        } else if (day > 5 || (time < 9 || time > 17)) {
             throw new BadSubmitDate("Can't submit issue on the submitted date!");
         }
         Calendar calendar = Calendar.getInstance();
-        int turnAroundHour = submitDate.get(Calendar.HOUR_OF_DAY) + turnAroundTime;
+        int turnAroundHour = time + turnAroundTime;
         calendar.set(Calendar.HOUR_OF_DAY, turnAroundHour);
         return calendar;
     }
