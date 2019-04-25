207586150c0d2be31501922e3e68d6291f206de4
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 14:40:32 +0200

    checks for submit date time

--- src/main/java/com/emarsys/assignment/IssueService.java
+++ src/main/java/com/emarsys/assignment/IssueService.java
@@ -10,8 +10,9 @@ public class IssueService {
     public Calendar calculateDueDate(Calendar submitDate, int turnAroundTime) {
         if (turnAroundTime <= 0) {
             throw new BadTurnAroundTime("Bad turnaround time!");
-        } else if (submitDate.get(Calendar.DAY_OF_WEEK) > 5) {
-            throw new BadSubmitDate("Can't submit issue on weekends!");
+        } else if (submitDate.get(Calendar.DAY_OF_WEEK) > 5 ||
+                (submitDate.get(Calendar.HOUR_OF_DAY) < 9 || submitDate.get(Calendar.HOUR_OF_DAY) > 17)) {
+            throw new BadSubmitDate("Can't submit issue on the submitted date!");
         }
         Calendar calendar = Calendar.getInstance();
         int turnAroundHour = submitDate.get(Calendar.HOUR_OF_DAY) + turnAroundTime;
