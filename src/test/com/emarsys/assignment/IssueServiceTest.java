4e55816be1fa94aa3b848f902248c2416e37b173
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 12:08:12 +0200

    test class and smoke test created

--- src/test/com/emarsys/assignment/IssueServiceTest.java
+++ src/test/com/emarsys/assignment/IssueServiceTest.java
@@ -1,7 +1,14 @@
 package com.emarsys.assignment;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class IssueServiceTest {
+
+    @Test
+    public void smoke() {
+        assertTrue(true);
+    }
 
 }
