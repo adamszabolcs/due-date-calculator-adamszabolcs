53ac2fbed3046ca7d2822179358e249f04e8b06c
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 13:17:18 +0200

    first test case created, checks if new calendar adds the turnAroundTime

--- src/test/com/emarsys/assignment/IssueServiceTest.java
+++ src/test/com/emarsys/assignment/IssueServiceTest.java
@@ -1,14 +1,39 @@
 package com.emarsys.assignment;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IssueServiceTest {
+
+    static IssueService issueService;
+    static Calendar calendar;
+
+
+    @BeforeAll
+    public static void init() {
+        issueService = new IssueService();
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_WEEK, 3);
+        calendar.set(Calendar.HOUR_OF_DAY, 11);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+    }
+
 
     @Test
     public void smoke() {
         assertTrue(true);
     }
 
+    @Test()
+    public void checkForAddingHours() {
+        Calendar testCalendar = issueService.calculateDueDate(calendar, 2);
+        assertTrue(testCalendar.get(Calendar.HOUR_OF_DAY)-calendar.get(Calendar.HOUR_OF_DAY) == 2,
+                "Adding hours works");
+    }
 }
