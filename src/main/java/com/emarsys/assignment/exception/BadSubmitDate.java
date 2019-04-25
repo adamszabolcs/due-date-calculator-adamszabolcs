1508b96f4991fd864de9e2db75aa1090b2e31b68
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 14:31:47 +0200

    made constructors public

--- src/main/java/com/emarsys/assignment/exception/BadSubmitDate.java
+++ src/main/java/com/emarsys/assignment/exception/BadSubmitDate.java
@@ -2,11 +2,11 @@ package com.emarsys.assignment.exception;
 
 public class BadSubmitDate extends RuntimeException {
 
-    BadSubmitDate(String message) {
+    public BadSubmitDate(String message) {
         super(message);
     }
 
-    BadSubmitDate(String message, Throwable error) {
+    public BadSubmitDate(String message, Throwable error) {
         super(message, error);
     }
 
