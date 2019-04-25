bd79e9e18ea60340ebe53a49acc19db3cb3b337c
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-25 13:05:38 +0200

    classes created

--- src/main/java/com/emarsys/assignment/IssueService.java
+++ src/main/java/com/emarsys/assignment/IssueService.java
@@ -1,4 +1,11 @@
 package com.emarsys.assignment;
 
+import java.util.Calendar;
+
 public class IssueService {
+
+    public Calendar calculateDueDate(Calendar submitDate, int turnAroundTime) {
+        return Calendar.getInstance();
+    }
+
 }
